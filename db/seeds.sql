3af89df0b820bd2991e8f188c2b1fc3ebd330249
Elham Fabe <elhamfabe@gmail.com>
2020-08-28 09:17:36 -0400

    folder structure for eat-da-burger

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers
+(burger_name, devour)
+VALUES
+('Truffle Burger',  false);
+INSERT INTO burgers
+(burger_name, devour)
+VALUES
+('Veggie Burger',  false);
+INSERT INTO burgers
+(burger_name, devour)
+VALUES
+('Ultimate Mac n Cheese burger', true);
+INSERT INTO burgers
+(burger_name, devour)
+VALUES
+('Cheeseburger',  false);
