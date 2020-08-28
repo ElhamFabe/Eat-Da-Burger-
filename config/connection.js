fc492971b7bc606963133a689a25aa4eb9d187d8
Elham Fabe <elhamfabe@gmail.com>
2020-08-28 09:28:22 -0400

    connection.js update

---
+++ config/connection.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+
+require('dotenv').config();
+// console.log("process.env", process.env)
+
+
+// create connection information for sql databaase
+const connection = mysql.createConnection({
+    // host
+    host: "localhost",
+
+    // Port
+    Port: 3306,
+    // user
+    user: "root",
+
+    // password
+    password: process.env.MYSQL_KEY,
+
+    // database
+    database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+
+// Export connection for our ORM to use.
+module.exports = connection;
